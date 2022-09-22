3716bd3c91467e1cef3dbf6cc9d1fa001edf6121
Bastian <112686261+bastianpr@users.noreply.github.com>
2022-09-22 16:38:12 +0700

    Add files via upload

---
+++ BastianPermataRomadon_C.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+using namespace std;
+int main(){
+	int u;
+	int w;
+	int x;
+	int z;
+	int y;
+	
+	u = 1 + 2;
+	w = 3 * 2 - 1;
+	x = 6 / 2;
+	z = 6 % 3;
+	
+  cout << "Isi nilai apapun" << endl;
+  cin >> y;
+   
+  cout << "u: " << u << endl;
+  cout << "w: " << w << endl;
+  cout << "x: " << x << endl;
+  cout << "z: " << z << endl;
+  cout << "Keseluruhan nilai yang tersedia ditambah nilai yang dimasukkan (y) : " << y + z + x + w + u << endl;
+  
+  
+  
+  return 0;
+}
